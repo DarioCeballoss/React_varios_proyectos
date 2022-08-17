5d4bfa7541c071fffe2a5b6f0ee500831215436e
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-17 19:42:58 -0300

    creando carpeta amount

---
+++ counter-redux/src/components/counter.js
@@ -0,0 +1,10 @@
+const Counter = () => {
+    return (
+        <div>
+            <h1>Amount ...</h1>
+            <button>Deposit $10</button>
+            <button>Withadraw $10</button>
+        </div>
+    );
+}
+export default Counter;
