eae66ae5abaf818358314399573f22508591454b
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-17 23:18:39 -0300

    creando Fin Proyecto

--- counter-redux/src/components/counter.js
+++ counter-redux/src/components/counter.js
@@ -1,10 +1,21 @@
-const Counter = () => {
+import { deposit, withdraw } from "../store/amount/action";
+import {connect} from "react-redux";
+
+const mapStateToProps = (state) => {
+    return {
+        amount: state.amountReducer.amount
+    };
+};
+
+const Counter = ({amount, deposit, withdraw}) => {
     return (
         <div>
-            <h1>Amount ...</h1>
-            <button>Deposit $10</button>
-            <button>Withadraw $10</button>
+            <h1>{amount}</h1>
+            <button onClick={()=> deposit()} >Deposit $10</button>
+            <button onClick={()=> withdraw()} >Withdraw $10</button>
         </div>
     );
-}
-export default Counter;
+};
+
+
+export default connect(mapStateToProps, {deposit, withdraw})(Counter);
