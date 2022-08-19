a6d257d0963310aab9aab58a9b792077efd20fc5
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-19 19:51:33 -0300

    add material

--- linkedin-clone-yt/src/App.js
+++ linkedin-clone-yt/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Vamos a darle</h1>
-      
-      {/* Header */}
+     
+      <Header />
       
       {/* App Body */}
       {/* Sidebar */}
---
+++ linkedin-clone-yt/src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+
+function Header()  {
+  return (
+    <div className='header'>
+      <h1>Este es el Header</h1>
+      
+      <div className='header_left'>
+        <img src='' alt='' />
+        <div className='header_search'>
+
+        </div>
+      </div>      
+
+      <div className='header_right'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header;
+//RFCE
+
