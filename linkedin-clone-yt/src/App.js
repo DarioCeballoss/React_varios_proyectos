6c9027061d7287a75a56895400baf1eef9e52df1
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-26 15:53:04 -0300

    Feed Input

--- linkedin-clone-yt/src/App.js
+++ linkedin-clone-yt/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
     <div className="App">
 
       <Header />
-
-      {/* App Body */}
+      
       <div className='app_body'>
         <Sidebar />
+        <Feed />
       </div>
-      {/* Sidebar */}
-      {/* Feed */}
+
       {/* Widgets */}
     </div>
   );
