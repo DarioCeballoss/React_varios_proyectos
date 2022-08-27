4554ab8d75c63f38963763acad021ec4d8cfd37a
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-26 22:10:00 -0300

    Feed Post

--- linkedin-clone-yt/src/Feed.js
+++ linkedin-clone-yt/src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from './InputOption';
@@ -6,26 +6,44 @@ import ImageIcon from "@material-ui/icons/Image";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventIcon from "@material-ui/icons/Event";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from './Post';
 
 function Feed() {
+
+  const [posts, setPosts] = useState([]);
+  const sendPost = e => {
+    e.preventDefault();
+
+  };
+
   return (
     <div className='feed'>
       <div className='feed_inputContainer'>
         <div className='feed_input'>
-        <CreateIcon />
-        <form>
-          <input type='text' />
-          <button type='submit'>Send</button>
-        </form>
+          <CreateIcon />
+          <form>
+            <input type='text' />
+            <button onClick={sendPost} type='submit'>Send</button>
+          </form>
         </div>
         <div className='feed_inputOptions'>
-          <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9'/>
-          <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E'/>
-          <InputOption Icon={EventIcon} title='Event' color='#C0CBCD'/>
-          <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E'/>
+          <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9' />
+          <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E' />
+          <InputOption Icon={EventIcon} title='Event' color='#C0CBCD' />
+          <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E' />
 
         </div>
       </div>
+
+      {posts.map((post)=>(
+        <Post />
+      ))}
+
+      <Post
+        name='Dario Ceballos'
+        description='esto es una prueva'
+        messaje='esto está funcionando'
+      />
     </div>
   );
 }
