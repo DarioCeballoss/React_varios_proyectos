2c876db811bc1b719c400585b989592b2b38e8f3
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-28 11:06:24 -0300

    inicio de ejemplo

---
+++ ejemplo-firestore/Dot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dot() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Dot;
---
+++ ejemplo-firestore/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <button className='button'>NEW</button>
+      <ul>
+        <li>
+          <a href='#'>
+            edit <Dot /> Red
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
