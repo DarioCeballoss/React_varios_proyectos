60345da5ab7b2603e570fea27c3a16ed72585749
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-08-28 21:03:12 -0300

    firestore linkedin ok

--- linkedin-clone-yt/src/Feed.js
+++ linkedin-clone-yt/src/Feed.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from './InputOption';
@@ -7,30 +7,47 @@ import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventIcon from "@material-ui/icons/Event";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from './Post';
-import { collection, doc, onSnapshot } from "firebase/firestore";
-import {db} from './firebase';
+import { collection, onSnapshot, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from './firebase';
 
 
 function Feed() {
-const [posts, setPosts] = useState([]);
-////////////////
-useEffect(() => {
-  onSnapshot(collection(db, 'posts'),(snapshot)=>{
-console.log(snapshot.docs);
+  const [input, setInput] = useState('');
+  const [posts, setPosts] = useState([]);
+  ///////  OK  /////////
+  useEffect(() => {
+    onSnapshot(collection(db, 'posts'), (snapshot) => {
+      setPosts(snapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data()
+      }
+      )));
+    });
   });
-});
-//////////////////////
-const sendPost = (e) => {
-  e.preventDefault();
-};
-////////////////////////////
+  /////////OK///////////
+  const sendPost = (e) => {
+    e.preventDefault();
+    try {
+      const docRef = addDoc(collection(db, 'posts'), {
+        name: 'Dario Ceballos',
+        description: 'pruevaFirebase',
+        messaje: input,
+        photoUrl: '',
+        timestamp: serverTimestamp()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+  ////////////////////////////
   return (
     <div className='feed'>
       <div className='feed_inputContainer'>
         <div className='feed_input'>
           <CreateIcon />
           <form>
-            <input type='text' />
+            <input value={input} onChange={e => setInput(e.target.value)} type='text' />
             <button onClick={sendPost} type='submit'>Send</button>
           </form>
         </div>
@@ -42,12 +59,16 @@ const sendPost = (e) => {
 
         </div>
       </div>
+      {posts.map(({ id, data: { name, description, messaje, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          messaje={messaje}
+          photoUrl={photoUrl}
+        />
+      ))}
 
-      <Post
-        name='Dario Ceballos'
-        description='esto es una prueva'
-        messaje='esto está funcionando'
-      />
     </div>
   );
 }
