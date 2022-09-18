d71c8177af4cbf6e95b6c7384a3d315e2862d134
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-09-18 09:35:30 -0300

    SENATORS Coneccion a endpoint con axios

--- senators/src/components/HouseData.js
+++ senators/src/components/HouseData.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function HouseData() {
+  const [member, setMember] = useState([]);
+
+
+  useEffect(() => {
+    const endPoint = 'https://api.propublica.org/congress/v1/116/senate/members.json';
+    axios.defaults.headers['X-API-KEY'] = 'rDYUl6e93qumCwukl6dAaoa8TUr4c6Nt52clfUF';
+
+    axios.get(endPoint)
+      .then(response => {
+        const apiData = response.data;
+        setMember(apiData.results[0].members);
+
+      }).catch((error) => {
+        console.log(error);
+      })
+  }, [member]);
+
+
   return (
-    <div>
-      huse2
-    </div>
+    <section>
+      <h2>Congressmen</h2>
+      <p>The major power of the House is to pass federal legislation that affects the entire country, although its
+        bills must also be passed by the Senate and further agreed to by the U.S. President before becoming law
+        (unless both the House and Senate re-pass the legislation with a two-thirds majority in each chamber). The
+        House has some exclusive powers: the power to initiate revenue bills, to impeach officials (impeached
+        officials are subsequently tried in the Senate), and to elect the U.S. President in case there is no
+        majority in the Electoral College.</p>
+      <p>Each U.S. state is represented in the House in proportion to its population as measured in the census, but
+        every state is entitled to at least one representative. </p>
+      <table className="table-borderless text-center">
+        <thead>
+          <tr>
+            <th>FULL NAME</th>
+            <th>PARTY</th>
+            <th>STATE</th>
+            <th>SENIORITY</th>
+            <th>PERCENTAGE OF VOTES</th>
+          </tr>
+        </thead>
+        <tbody >
+          {/*aca va el map*/}
+
+          {member.map((member, ind) => {
+            return (
+              <tr key={ind}>
+                <td><a href={member.url}>{member.last_name} {member.first_name} {member.middle_name}</a></td>
+                <td>{member.party}</td>
+                <td>{member.state}</td>
+                <td>{member.seniority}</td>
+                <td>{member.votes_with_party_pct}</td>
+              </tr>
+            )
+          })}
+
+        </tbody>
+      </table>
+    </section>
   )
 }
 
