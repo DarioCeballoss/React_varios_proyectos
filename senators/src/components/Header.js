2690217543671eb6d981bdb63cc84549a09ea9e0
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-09-17 23:31:49 -0300

    SENATORS NavBar dropdown

--- senators/src/components/Header.js
+++ senators/src/components/Header.js
@@ -7,33 +7,55 @@ function Header() {
 
 	return (
 		<header >
-			<div className="clearfix">
-				<div className="float-left">
-					<Link to="https://www.whitehouse.gov/the_press_office/TransparencyandOpenGovernment"><img src={LogoTgif}
-						alt="TGIFlogo" className="logo" /> </Link>
-				</div>
-				<div className="float-right">
-					<Link href="mailto:info@tgif.net?Subject=Hello%20again" target="_top">info@tgif.net</Link>
-				</div>
+			<div className="header-img">
+
+				<a href="https://www.whitehouse.gov/the_press_office/TransparencyandOpenGovernment">
+					<img src={LogoTgif} alt="TGIFlogo" className="logo" />
+				</a>
+
+				<a href="mailto:info@tgif.net?Subject=Hello%20again" target="_top">info@tgif.net</a>
+
 			</div>
+
 			<nav className="sticky-top row  col navbar navbar-expand-sm bg-light navbar-light ">
 				<div>
 
 					<ul className="navbar-nav">
-						<li className="nav-item"><Link to='/' className="nav-link" ><b>Home</b></Link></li>
+						<li className="nav-item">
+							<Link to='/' className="nav-link" ><b>Home</b></Link>
+						</li>
 
-						<li className="nav-item"><Link to='/senate' className="nav-link"><b>Senate</b></Link></li>
-						<li className="nav-item"><Link to='/house' className="nav-link"><b>House</b></Link></li>
+						<li className="nav-item dropdown">
+							<button className="dropbtn nav-link dropdown-toggle bg-light border-0" >
+								<b>Congress 113</b>
+							</button>
+							<ul className="dropdown-menu dropdown-content">
+								<li><Link to='/senate' className="dropdown-item"><b>Senate</b></Link></li>
+								<li><Link to='/house' className="dropdown-item"><b>House</b></Link></li>
+							</ul>
+						</li>
 
-						<li className="nav-item"><Link to='/house/attendancce' className="nav-link"><b> Attendance House</b></Link></li>
-						<li className="nav-item"><Link to='/senate/attendancce' className="nav-link"><b>Attendance Senate</b></Link></li>
+						<li className="nav-item dropdown">
+							<button className="dropbtn nav-link dropdown-toggle bg-light border-0" >
+								<b>Attendance</b>
+							</button>
+							<ul className="dropdown-menu dropdown-content">
+								<li><Link to='/house/attendancce' className="dropdown-item"><b>House</b></Link></li>
+								<li><Link to='/senate/attendancce' className="dropdown-item"><b>Senate</b></Link></li>
+							</ul>
+						</li>
 
-
-						<li className="nav-item"><Link to='/house/partyloyalty' className="nav-link"><b>Party Loyalty House</b></Link></li>
-						<li className="nav-item"><Link to='/senate/partyloyalty' className="nav-link"><b>Party Loyalty Senate</b></Link></li>
+						<li className="nav-item dropdown">
+							<button className="dropbtn nav-link dropdown-toggle bg-light border-0" >
+								<b>Party Loyalty</b>
+							</button>
+							<ul className="dropdown-menu dropdown-content">
+								<li><Link to='/house/partyloyalty' className="dropdown-item"><b>House</b></Link></li>
+								<li><Link to='/senate/partyloyalty' className="dropdown-item"><b>Senate</b></Link></li>
+							</ul>
+						</li>
 					</ul>
-
-
+					
 				</div>
 			</nav>
 		</header>
